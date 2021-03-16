dd01144d4db09c22736398d070f4a690868e2e1d
Julien Dubois <74368850+ChuisJu@users.noreply.github.com>
2021-03-16 19:07:10 +0100

    Add files via upload

---
+++ controleur/StatEtab.java
@@ -0,0 +1,164 @@
+package controleur;
+
+import java.util.ArrayList;
+
+import model.Etablissement;
+
+/**
+ * Calculs statistiques sur les données de pollution des 
+ * polluants N02, PM10 et PM25
+ * @author Flavie Tonon
+ *
+ */
+public class StatEtab {
+
+	/**
+	 * Calcule la moyenne du polluant N02 pour un département donné
+	 * @param l La liste des établissements
+	 * @param dpt Le nom du département
+	 * @param annee L'année de récolte des données demandée
+	 * @return la moyenne de pollution
+	 * Flavie Tonon
+	 */
+	public static double getMoyennePolluantNO2Dpt(ArrayList<Etablissement> l, String dpt, int annee) {
+		double moy = 0;
+		for(Etablissement e : l) {
+			if(e.getLieu().getDepartement().equals(dpt)) {
+				moy+=e.getPollutionNO2(annee);
+			}
+		}
+		return moy/l.size();
+	}
+
+	/**
+	 * Calcule la moyenne du polluant PM10 pour un département donné
+	 * @param l La liste des établissements
+	 * @param dpt Le nom du département
+	 * @param annee L'année de récolte des données demandée
+	 * @return la moyenne de pollution
+	 * Flavie Tonon
+	 */
+	public static double getMoyennePolluantPM10Dpt(ArrayList<Etablissement> l, String dpt, int annee) {
+		double moy = 0;
+		for(Etablissement e : l) {
+			if(e.getLieu().getDepartement().equals(dpt)) {
+				moy+=e.getPollutionPM10(annee);
+			}
+		}
+		return moy/l.size();
+	}
+
+	/**
+	 * Calcule la moyenne du polluant PM25 pour un département donné
+	 * @param l La liste des établissements
+	 * @param dpt Le nom du département
+	 * @param annee L'année de récolte des données demandée
+	 * @return la moyenne de pollution
+	 * Flavie Tonon
+	 */
+	public static double getMoyennePolluantPM25Dpt(ArrayList<Etablissement> l, String dpt, int annee) {
+		double moy = 0;
+		for(Etablissement e : l) {
+			if(e.getLieu().getDepartement().equals(dpt)) {
+				moy+=e.getPollutionPM25(annee);
+			}
+		}
+		return moy/l.size();
+	}
+	
+
+	/**
+	 * Calcule la moyenne du polluant N02 pour une ville donnée
+	 * @param l La liste des établissements
+	 * @param ville Le nom de la ville
+	 * @param annee L'année de récolte des données demandée
+	 * @return la moyenne de pollution
+	 * Flavie Tonon
+	 */
+	public static double getMoyennePolluantNO2Ville(ArrayList<Etablissement> l, String ville, int annee) {
+		double moy = 0;
+		for(Etablissement e : l) {
+			if(e.getLieu().getVille().equals(ville)) {
+				moy+=e.getPollutionNO2(annee);
+			}
+		}
+		return moy/l.size();
+	}
+
+	/**
+	 * Calcule la moyenne du polluant PM10 pour une ville donnée
+	 * @param l La liste des établissements
+	 * @param ville Le nom de la ville
+	 * @param annee L'année de récolte des données demandée
+	 * @return la moyenne de pollution
+	 * Flavie Tonon
+	 */
+	public static double getMoyennePolluantPM10Ville(ArrayList<Etablissement> l, String ville, int annee) {
+		double moy = 0;
+		for(Etablissement e : l) {
+			if(e.getLieu().getVille().equals(ville)) {
+				moy+=e.getPollutionPM10(annee);
+			}
+		}
+		return moy/l.size();
+	}
+	
+	/**
+	 * Calcule la moyenne du polluant PM25 pour une ville donn�e
+	 * @param l La liste des �tablissements
+	 * @param ville Le nom de la ville
+	 * @param annee L'ann�e de r�colte des donn�es demand�e
+	 * @return la moyenne de pollution
+	 * Flavie Tonon
+	 */
+	public static double getMoyennePolluantPM25Ville(ArrayList<Etablissement> l, String ville, int annee) {
+		double moy = 0;
+		for(Etablissement e : l) {
+			if(e.getLieu().getVille().equals(ville)) {
+				moy+=e.getPollutionPM25(annee);
+			}
+		}
+		return moy/l.size();
+	}
+	
+	/**
+	 * Trouve l'�tablissement le plus polluant au NO2 sur une ann�e
+	 * @param l La liste des �tablissements
+	 * @param annee L'ann�e de r�colte des donn�es demand�e
+	 * @return L'�tablissement le plus polluant au N02
+	 * Flavie Tonon
+	 */
+	public static Etablissement getPlusPolluantNO2(ArrayList<Etablissement> l, int annee) {
+		//TODO Compléter getPlusPolluantNO2 
+		for ( int i = 2012; i < 2017; i++) {
+			for (Etablissement e : l) {
+				if (i.equals(e.getPollutionNO2()) {
+					
+				}
+			}
+		}
+		return null;
+	}
+	/**
+	 * Trouve l'�tablissement le plus polluant au PM10 sur une ann�e
+	 * @param l La liste des �tablissements
+	 * @param annee L'ann�e de r�colte des donn�es demand�e
+	 * @return L'�tablissement le plus polluant au PM10
+	 * Flavie Tonon
+	 */
+	public static Etablissement getPlusPolluantPM10(ArrayList<Etablissement> l, int annee) {
+		//TODO Compl�ter getPlusPolluantPM10 
+		return null;
+	}
+	/**
+	 * Trouve l'établissement le plus polluant au PM25 sur une année
+	 * @param l La liste des établissements
+	 * @param annee L'année de récolte des données demandée
+	 * @return L'établissement le plus polluant au PM25
+	 * Flavie Tonon
+	 */
+	public static Etablissement getPlusPolluantPM25(ArrayList<Etablissement> l, int annee) {
+		//TODO Compléter getPlusPolluantPM25 
+		return null;
+	}
+}
